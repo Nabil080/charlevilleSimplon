129e84579c3a47d9e14d2511f57498fd229b2750
Florian Gaudin <120341138+Florian-Gaudin@users.noreply.github.com>
2023-05-05 10:02:53 +0200

    Update layout.php

--- view/layout.php
+++ view/layout.php
@@ -4,17 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simplon.co - <?= $title ?></title>
-    <link href="https://fonts.googleapis.com/css2?family=Lato&family=Roboto&display=swap" rel="stylesheet">
+    <title>Simplon.co -
+        <?= $title ?>
+    </title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
     tailwind.config = {
         theme: {
-            fontFamily: {
-              main : 'Roboto',
-              title : 'Montserrat',
-            },
             extend: {
+                fontFamily: {
+                    'main' : ['"Roboto"', 'sans-serif'],
+                    'title' : ['"Montserrat"', 'sans-serif'],
+                },
                 colors: {
                     'main':{
                         red:'#da373d',
