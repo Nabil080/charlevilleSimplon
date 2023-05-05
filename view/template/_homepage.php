7342b61b69ef10c118bead560723962a74728605
Nabil BELLILA <bellilanabil8@gmail.com>
2023-05-05 16:16:10 +0200

    vidéo compressée

--- view/template/_homepage.php
+++ view/template/_homepage.php
@@ -20,7 +20,7 @@ ob_start();
         <h1 class="hidden lg:block uppercase font-title text-main-white w-fit mx-auto -translate-y-8 text-[60px] font-bold text-center">Simplon <br> charleville-mézières</h1>
         <section id="homepage" class="ease-in duration-300 lg:mt-20 2xl:mt-36 w-full [&_*]:mx-auto relative
         [&_button]:block [&_button]:max-w-[410px] [&_button]:bg-main-red [&_button]:w-3/4 [&_button]:py-8 [&_button]:lg:py-20 [&_button]:xl:py-24  [&_button]:my-12 [&_button]:border-main-white [&_button]:border-2 [&_button]:text-main-white [&_button]:font-title [&_button]:font-bold [&_button]:uppercase [&_button]:text-xl [&_button]:lg:text-3xl [&_button]:xl:text-[40px] [&_button]:ease-linear [&_button]:duration-200">
-            <h2 class="w-fit font-title font-bold text-3xl text-main-white">Je suis</h2>
+            <h2 class="w-fit font-title font-bold text-3xl text-main-white xl:text-[46px]">Je suis</h2>
             <div class="lg:flex gap-8 px-8">
                 <button onclick="companyHomepage()" type="button" class="hover:bg-main-white hover:text-main-red hover:border-main-red hover:-translate-y-4">Une entreprise</button>
                 <button onclick="loginModal()" type="button" class="hover:bg-main-white hover:text-main-red hover:border-main-red hover:-translate-y-4 ">Un apprenant</button>
@@ -31,8 +31,8 @@ ob_start();
         <!-- div entreprise -->
         <section id="company-homepage" class="opacity-0 hidden lg:mt-20 2xl:mt-36 ease-in duration-300 w-full [&>*]:mx-auto relative
         [&_button]:block [&_button]:max-w-[410px] [&_button]:bg-main-red [&_button]:w-3/4 [&_button]:py-8 [&_button]:lg:py-20 [&_button]:xl:py-24  [&_button]:my-12 [&_button]:border-main-white [&_button]:border-2 [&_button]:text-main-white [&_button]:font-title [&_button]:font-bold [&_button]:uppercase [&_button]:text-xl [&_button]:lg:text-3xl [&_button]:xl:text-[40px] [&_button]:ease-linear [&_button]:duration-200">
-            <h2 class="absolute -top-10 w-full text-center font-title font-bold text-3xl text-main-white">Je suis une entreprise</h2>
-            <h2 class="w-fit font-title font-bold text-3xl text-main-white">Je souhaite</h2>
+            <h2 class="absolute -top-12 w-full text-center font-title font-bold text-3xl text-main-white xl:text-[46px]">Je suis une entreprise</h2>
+            <h2 class="w-fit font-title font-bold text-3xl text-main-white xl:text-[46px]">Je souhaite</h2>
             <div class="lg:flex gap-8 px-8 [&>button]:mx-auto [&>button]:xl:mx-0">
                 <button onclick="companyPromo()" type="button" class="hover:bg-main-white hover:text-main-red hover:border-main-red hover:-translate-y-4 ">Chercher un profil</button>
                 <button onclick="companyProject()" type="button" class="hover:bg-main-white hover:text-main-red hover:border-main-red hover:-translate-y-4 ">Soumettre un projet</button>
@@ -43,8 +43,8 @@ ob_start();
         <!-- div visiteur -->
         <section id="visitor-homepage" class="opacity-0 hidden lg:mt-20 2xl:mt-36 ease-in duration-300 w-full [&>*]:mx-auto relative
         [&_button]:block [&_button]:max-w-[410px] [&_button]:bg-main-red [&_button]:w-3/4 [&_button]:py-8 [&_button]:lg:py-20 [&_button]:xl:py-24  [&_button]:my-12 [&_button]:border-main-white [&_button]:border-2 [&_button]:text-main-white [&_button]:font-title [&_button]:font-bold [&_button]:uppercase [&_button]:text-xl [&_button]:lg:text-3xl [&_button]:xl:text-[40px] [&_button]:ease-linear [&_button]:duration-200">
-            <h2 class="absolute -top-10 w-full text-center font-title font-bold text-3xl text-main-white">Je suis un visiteur</h2>
-            <h2 class="w-fit font-title font-bold text-3xl text-main-white">Je souhaite découvrir</h2>
+            <h2 class="absolute -top-12 w-full text-center font-title font-bold text-3xl text-main-white xl:text-[46px]">Je suis un visiteur</h2>
+            <h2 class="w-fit font-title font-bold text-3xl text-main-white xl:text-[46px]">Je souhaite découvrir</h2>
             <div class="lg:flex gap-8 px-8 [&>button]:mx-auto [&>button]:xl:mx-0 justify-center">
                 <button onclick="visitorFormation()" type="button" class="hover:bg-main-white hover:text-main-red hover:border-main-red hover:-translate-y-4 ">Les formations</button>
                 <button onclick="visitorProject()" type="button" class="hover:bg-main-white hover:text-main-red hover:border-main-red hover:-translate-y-4 ">Les projets</button>
