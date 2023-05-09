a6d2b3609340a4477145348a1c0007452a198f92
Nabil BELLILA <bellilanabil8@gmail.com>
2023-05-09 16:05:59 +0200

    contenu tab responsif

--- view/template/_project.php
+++ view/template/_project.php
@@ -85,27 +85,44 @@
     </div>
 </section>
 
-<!-- CONTENU TAB ENTREPRISE -->
-<section class="sectionChange">
-    <h2 class="text-center italic font-title text-xl lg:text-2xl font-semibold uppercase my-4">Demandes de l'entreprise</h2>
-    <div class="mx-4 h-1 bg-main-red"></div>
-    <p class="m-4">La méthode d’apprentissage <I>Simplon.co</I>, <B>c’est une pédagogie active et par projets</B> où l’apprenant est confronté à des mises en situation concrètes via des projets individuels et en groupe. Les thèmes des projets sont directement issus de cas réels, pour permettre aux apprenants d’appréhender le terrain dès la formation. Le collectif est un élément central de l’apprentissage car <B>le travail en équipe est constant</B>, et les apprenants s’entraident et s’évaluent régulièrement.<br><br>
-​​​​​​​
-Pour garantir une <B>insertion professionnelle réussie</B>, l’apprenante est accompagnée par des formateurs experts du métier visé et des équipes en lien avec les entreprises du territoire. Il est immergé dans le monde de l’entreprise tout au long de sa formation : parrainage de la promotion par une entreprise, simulations d’entretiens, meet-ups...<br><br>
-​​​​​​​
-Quasiment toutes nos formations débouchent sur <B>la délivrance de certifications reconnues</B>, soit un Titre RNCP reconnu par le ministère du travail (équivalent diplôme), soit une ou plusieurs certifications Simplon reconnues par l'État ou par les professionnels.</p>
-</section>
-<!-- CONTENU TAB APPRENANT -->
-<section class="sectionChange hidden">
-    <h2 class="text-center italic font-title text-xl lg:text-2xl font-semibold uppercase my-4">Notes des apprenants</h2>
-    <div class="mx-4 h-1 bg-main-red"></div>
-    <p class="m-4">La méthode d’apprentissage <I>Simplon.co</I>, <B>c’est une pédagogie active et par projets</B> où l’apprenant est confronté à des mises en situation concrètes via des projets individuels et en groupe. Les thèmes des projets sont directement issus de cas réels, pour permettre aux apprenants d’appréhender le terrain dès la formation. Le collectif est un élément central de l’apprentissage car <B>le travail en équipe est constant</B>, et les apprenants s’entraident et s’évaluent régulièrement.<br><br>
-​​​​​​​
-Pour garantir une <B>insertion professionnelle réussie</B>, l’apprenante est accompagnée par des formateurs experts du métier visé et des équipes en lien avec les entreprises du territoire. Il est immergé dans le monde de l’entreprise tout au long de sa formation : parrainage de la promotion par une entreprise, simulations d’entretiens, meet-ups...<br><br>
-​​​​​​​
-Quasiment toutes nos formations débouchent sur <B>la délivrance de certifications reconnues</B>, soit un Titre RNCP reconnu par le ministère du travail (équivalent diplôme), soit une ou plusieurs certifications Simplon reconnues par l'État ou par les professionnels.</p>
-</section>
 
+<!-- CONTENU TAB -->
+<section class="max-w-[1200px] mx-auto flex">
+    <!-- CONTENU TAB ENTREPRISE -->
+    <div class="sectionChange">
+        <h2 class="text-center italic font-title text-xl lg:text-2xl font-semibold uppercase my-4">Demandes de l'entreprise</h2>
+        <div class="mx-4 h-1 bg-main-red"></div>
+        <div class="flex">
+            <p class="m-4 lg:w-4/5">
+                La méthode d'apprentissage <I>Simplon.co</I>, <B>c'est une pédagogie active et par projets</B> où l'apprenant est confronté à des mises en situation concrètes via des projets individuels et en groupe. Les thèmes des projets sont directement issus de cas réels, pour permettre aux apprenants d'appréhender le terrain dès la formation. Le collectif est un élément central de l'apprentissage car <B>le travail en équipe est constant</B>, et les apprenants s'entraident et s'évaluent régulièrement.<br><br>
+
+                Pour garantir une <B>insertion professionnelle réussie</B>, l'apprenante est accompagnée par des formateurs experts du métier visé et des équipes en lien avec les entreprises du territoire. Il est immergé dans le monde de l'entreprise tout au long de sa formation : parrainage de la promotion par une entreprise, simulations d'entretiens, meet-ups...<br><br>
+
+                Quasiment toutes nos formations débouchent sur <B>la délivrance de certifications reconnues</B>, soit un Titre RNCP reconnu par le ministère du travail (équivalent diplôme), soit une ou plusieurs certifications Simplon reconnues par l'État ou par les professionnels.
+            </p>
+            <!-- ANIMATION ASIDE -->
+            <div class="hidden lg:grid w-1/5 place-items-center">
+                <img class="w-4/5"src="assets/img/logo" alt="anim">
+            </div>
+        </div>
+    </div>
+    <!-- CONTENU TAB APPRENANT -->
+    <div class="sectionChange hidden">
+        <h2 class="text-center italic font-title text-xl lg:text-2xl font-semibold uppercase my-4">Notes des apprenants</h2>
+        <div class="mx-4 h-1 bg-main-red"></div>
+        <div class="flex">
+            <p class="m-4 lg:w-4/5">
+                La méthode d'apprentissage <I>Simplon.co</I>, <B>c'est une pédagogie active et par projets</B> où l'apprenant est confronté à des mises en situation concrètes via des projets individuels et en groupe. Les thèmes des projets sont directement issus de cas réels, pour permettre aux apprenants d'appréhender le terrain dès la formation. Le collectif est un élément central de l'apprentissage car <B>le travail en équipe est constant</B>, et les apprenants s'entraident et s'évaluent régulièrement.<br><br>
+
+                Pour garantir une <B>insertion professionnelle réussie</B>, l'apprenante est accompagnée par des formateurs experts du métier visé et des équipes en lien avec les entreprises du territoire. Il est immergé dans le monde de l'entreprise tout au long de sa formation : parrainage de la promotion par une entreprise, simulations d'entretiens, meet-ups...<br><br>
+            </p>
+            <!-- ANIMATION ASIDE -->
+            <div class="hidden lg:grid w-1/5 place-items-center">
+                <img class="w-4/5"src="assets/img/logo" alt="anim">
+            </div>
+        </div>
+    </div>
+</section>
 
 
 
