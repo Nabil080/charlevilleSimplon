28bba283e785f829a02643894f8fbb7b1dad4774
Nabil BELLILA <bellilanabil8@gmail.com>
2023-05-09 15:35:41 +0200

    header + aperçu responsif

--- view/template/_project.php
+++ view/template/_project.php
@@ -4,10 +4,10 @@
 
 <!-- header -->
 <section class="text-center [&_h5]:italic [&_h5]:my-2">
-    <h1 class="font-title text-2xl font-semibold text-main-red uppercase">Super projet de zinzin</h1>
+    <h1 class="font-title text-2xl sm:text-3xl lg:text-[40px] font-semibold text-main-red uppercase">Super projet de zinzin</h1>
     <div>
         <h5>Réalisé par :</h5>
-        <div class="flex flex-wrap justify-center gap-2 text-sm text-main-white [&>a]:bg-main-gray [&>a]:py-0.5 [&>a]:px-3 [&>a]:rounded-full">
+        <div class="flex flex-wrap justify-center gap-2 text-sm md:text-base 2xl:text-lg text-main-white [&>a]:bg-main-gray [&>a]:py-0.5 [&>a]:px-3 [&>a]:rounded-full">
             <a href="profil de l'apprenant" class="hover:border border-main-gray hover:text-main-gray hover:bg-main-white">Nabil</a>
             <a href="profil de l'apprenant" class="hover:border border-main-gray hover:text-main-gray hover:bg-main-white">Alexandre</a>
             <a href="profil de l'apprenant" class="hover:border border-main-gray hover:text-main-gray hover:bg-main-white">Florian</a>
@@ -16,7 +16,7 @@
     </div>
     <div>
         <h5>Supervisé par :</h5>
-        <div class="flex flex-wrap justify-center gap-2 text-sm text-main-white [&>a]:bg-main-gray [&>a]:py-0.5 [&>a]:px-3 [&>a]:rounded-full">
+        <div class="flex flex-wrap justify-center gap-2 text-sm md:text-base 2xl:text-lg text-main-white [&>a]:bg-main-gray [&>a]:py-0.5 [&>a]:px-3 [&>a]:rounded-full">
             <a href="profil de l'apprenant" class="hover:border border-main-gray hover:text-main-gray hover:bg-main-white">Axel</a>
             <a href="profil de l'apprenant" class="hover:border border-main-gray hover:text-main-gray hover:bg-main-white">Steven</a>
         </div>
@@ -24,29 +24,33 @@
 </section>
 
 <!-- aperçu projet -->
-<section class="text-center mx-4">
+<section class="text-center sm:text-start mx-4 mt-4 sm:grid grid-cols-2 gap-8 sm:gap-10 md:gap-12 lg:gap-20 xl:gap-40 max-w-[1200px] lg:mx-12 xl:mx-auto">
     <!-- maquette -->
-    <div class=""><div class="font-title text-xl font-bold my-4 underline w-full"><a href="lien de la maquette">Maquette/Wireframe</a></div>
-        <div class="w-full max-h-[200px] overflow-scroll"><img class="w-full" src="assets/img/maquette" alt="maquette du projet"></div>
-    </div>
+    <article class="order-2">
+        <div class="font-title text-xl sm:text-2xl xl:text-3xl font-bold my-4 underline w-full"><a href="lien de la maquette">Maquette/Wireframe</a></div>
+        <div class="w-full max-h-[200px] sm:max-h-[250px] overflow-scroll"><img class="w-full" src="assets/img/maquette" alt="maquette du projet"></div>
+    </article>
     <!-- avancement -->
-    <div class=""><div class="font-title text-xl font-bold my-4">Avancement du projet</div>
-        <!-- avancement 1 -->
-        <div class="mb-1 text-start italic">Cahier des charges</div>
-        <div class="w-full bg-gray-200 rounded-full h-2.5 mb-4  ">
-            <div class="bg-green-600 h-2.5 rounded-full" style="width: 100%"></div>
+    <article class="flex-col flex">
+        <div class="font-title text-xl sm:text-2xl xl:text-3xl font-bold my-4">Avancement</div>
+        <div class="grow flex flex-col justify-center">
+            <!-- avancement 1 -->
+            <div class="mb-1 text-start italic">Cahier des charges</div>
+            <div class="w-full bg-gray-200 rounded-full h-2.5 mb-4  ">
+                <div class="bg-green-600 h-2.5 rounded-full" style="width: 100%"></div>
+            </div>
+            <!-- avancement 2 -->
+            <div class="mb-1 text-start italic">Développement front</div>
+            <div class="w-full bg-gray-200 rounded-full h-2.5 mb-4  ">
+                <div class="bg-yellow-400 h-2.5 rounded-full" style="width: 60%"></div>
+            </div>
+            <!-- avancement 3 -->
+            <div class="mb-1 text-start italic">Non défini</div>
+            <div class="w-full bg-gray-200 rounded-full h-2.5 mb-4  ">
+                <div class="bg-red-main h-2.5 rounded-full" style="width: 45%"></div>
+            </div>
         </div>
-        <!-- avancement 2 -->
-        <div class="mb-1 text-start italic">Développement front</div>
-        <div class="w-full bg-gray-200 rounded-full h-2.5 mb-4  ">
-            <div class="bg-yellow-400 h-2.5 rounded-full" style="width: 60%"></div>
-        </div>
-        <!-- avancement 3 -->
-        <div class="mb-1 text-start italic">Non défini</div>
-        <div class="w-full bg-gray-200 rounded-full h-2.5 mb-4  ">
-            <div class="bg-red-main h-2.5 rounded-full" style="width: 45%"></div>
-        </div>
-    </div>
+    </article>
 </section>
 
 <!-- boutons liens externes -->
