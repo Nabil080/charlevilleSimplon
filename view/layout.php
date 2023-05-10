00ba859070473c3e413ccaa18ae64ed3f518615e
Florian Gaudin <floriangaudin13@gmail.com>
2023-05-10 13:45:59 +0200

    modif layout

--- view/layout.php
+++ view/layout.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html>
+<html class="w-[100vw] overflow-x-hidden">
 
 <head>
     <meta charset="UTF-8">
@@ -7,27 +7,28 @@
     <title>Simplon.co -
         <?= $title ?>
     </title>
-    <link href="https://fonts.googleapis.com/css2?family=Lato&family=Roboto&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/tw-elements.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/eb7aa99f8d.js" crossorigin="anonymous"></script>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://kit.fontawesome.com/eb7aa99f8d.js" crossorigin="anonymous"></script>
     <script>
     tailwind.config = {
         theme: {
+            fontFamily: {
+                sans: 'Roboto',
+                title: 'Montserrat',
+            },
             extend: {
-                fontFamily: {
-                    'main' : ['"Roboto"', 'sans-serif'],
-                    'title' : ['"Montserrat"', 'sans-serif'],
-                },
                 colors: {
                     'main':{
                         red:'#BD3124',
-                        white:'#EFF1F3',
+                        white:'#FFFF',
                         gray: '#4F4F4F',
                         lightred: '#F6DADE',
                         lightgray: '#F2F2F3',
-                        green: '#A2EF4D',
-                        border: '#BBBBBB',
+                        blue: '#4A9AE6',
                     }
                 }
             }
@@ -35,14 +36,18 @@
     }
     </script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
+    <link href="assets/css/nav.css" rel="stylesheet" />
+    <?php if (isset($link))
+        echo $link; ?>
 </head>
 
-<body>
+<body class="w-[100vw] 2xl:px-[5%] overflow-x-hidden bg-main-white">
+
     <?php require_once 'view/template/_navbar.php'; ?>
     <?= $content ?>
     <?php require_once 'view/template/_footer.php'; ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/tw-elements.umd.min.js"></script>
+    <script src="https://kit.fontawesome.com/eb7aa99f8d.js" crossorigin="anonymous"></script>
     <?php if (isset($script))
         echo $script; ?>
 </body>
