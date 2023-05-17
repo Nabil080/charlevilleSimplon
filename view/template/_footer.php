e066411f8eb85f6c50c8ab0c68597adea2778ef2
Nabil BELLILA <bellilanabil8@gmail.com>
2023-05-17 10:40:43 +0200

    fake footer

--- view/template/_footer.php
+++ view/template/_footer.php
@@ -1,7 +1,18 @@
-<footer class="w-[100vw] bg-main-white border-b-2 border-main-red py-4 mt-4 flex flex-wrap justify-center sm:justify-evenly gap-y-2 gap-x-4 lg:text-xl ">
-        <a href="#" class="text-main-red hover:underline">Contact</a>
-        <a href="#" class="text-main-red hover:underline">Plan du site</a>
-        <a href="#" class="text-main-red hover:underline">Copyright</a>
-        <a href="#" class="text-main-red hover:underline">Mentions légales</a>
-        <a href="#" class="text-main-red hover:underline">Entreprises partenaires</a>
+<footer class="w-[100vw] bg-main-white border-b-2 text-main-red border-main-red py-4 mt-4 flex flex-wrap justify-center sm:justify-evenly gap-y-2 gap-x-4 lg:text-xl ">
+        <!-- <a href="#" class="hover:underline">Contact</a>
+        <a href="#" class="hover:underline">Plan du site</a>
+        <a href="#" class="hover:underline">Copyright</a>
+        <a href="#" class="hover:underline">Mentions légales</a>
+        <a href="#" class="hover:underline">Entreprises partenaires</a> -->
+        <a href="?action=homepage" class="hover:underline">Accueil</a>
+        <a href="?action=allFormationsPage" class="hover:underline">Toutes les formations</a>
+        <a href="?action=allPromotionsPage" class="hover:underline">Toutes les promotions</a>
+        <a href="?action=allProjectsPage" class="hover:underline">Tous les projets</a>
+        <a href="?action=formationPage" class="hover:underline">Page Formation</a>
+        <a href="?action=promotionPage" class="hover:underline">Page Promotion</a>
+        <a href="?action=projectPage" class="hover:underline">Page Projet</a>
+        <a href="?action=contactPage" class="hover:underline">Page Contact</a>
+        <a href="?action=projectGestionPage" class="hover:underline">Page Gestion de Projet</a>
+        <a href="?action=registerPage" class="hover:underline">Page d'inscription</a>
+        <a href="?action=registerPage" class="hover:underline">Crud</a>
 </footer>
