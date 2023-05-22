b35fe0cdd2992aa044fd3b9803dd9869c2051f15
Alexandre D <derriere.alexandre0103@gmail.com>
2023-05-22 15:06:34 +0200

    add modal

--- assets/js/crud.js
+++ assets/js/crud.js
@@ -27,9 +27,57 @@ inputs[i].addEventListener('change', function()  // Event listener sur chaque ch
 
 window.addEventListener("scroll", function () { // Ecoute le scroll sur la page
 
-    if (nav.classList.contains("-translate-y-[20vh]")) { // si la navbar est caché fait remonté l'aside jusqu'en haut
+    if (nav.classList.contains("-translate-y-[20vh]") && this.window.screen.width > 762) { // si la navbar est caché fait remonté l'aside jusqu'en haut
         aside.classList.add("-translate-y-20");
     } else {    // Si la navbar apparait fait redescendre l'aside
         aside.classList.remove("-translate-y-20");
     }
 });
+
+// Gestion de validation de promotion
+
+const validationForm = document.getElementById("validationForm");
+const showValidation = document.getElementById("showValidation");
+const inputsValidation = validationForm.getElementsByTagName("input");
+let allText = [];
+let containerValidation = 0;
+
+for (i = 0; i < inputsValidation.length; i++) {
+    inputsValidation[i].addEventListener("change", (e) => {
+        let validation = document.getElementById(e.target.dataset.name);
+
+        if (validation == null) {
+            if (typeof containerValidation !== 'undefined' && containerValidation !== 0) {
+                containerValidation.remove();
+            }
+
+            showValidation.insertAdjacentHTML("afterbegin", "<div id='containerValidation'></div>");
+            containerValidation = document.getElementById("containerValidation");
+
+            inputs.forEach(input => {
+                console.log("salut");
+                if (input.checked) {
+                    console.log("salut");
+                    allText.push(input.dataset.name);
+                }
+            });
+
+            // let text = "<p id='" + e.target.dataset.name + "'>" + e.target.dataset.name + "</p>";
+            // allText.push(text);
+            allText = allText.sort();
+            finalText = allText.join("");
+
+            containerValidation.insertAdjacentHTML("afterbegin", finalText);
+        
+        } else {
+            console.log(validation.outerHTML);
+            console.log(allText[1]);
+
+            
+            validation.remove();
+            allText.splice(index);
+            console.log("cet élement existe déja");
+        }
+    });
+};
+
