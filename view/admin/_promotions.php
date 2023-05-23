d3f0a1a19a49badb4fa4d068d1a627c9c6402e12
Alexandre D <127296626+Alex080103@users.noreply.github.com>
2023-05-23 10:18:40 +0200

    Update _promotions.php

--- view/admin/_promotions.php
+++ view/admin/_promotions.php
@@ -5,6 +5,7 @@
 include("view/admin/crudPromotion.php");
 include("view/admin/modalApprenant.php");
 include("view/admin/modalFormateur.php");
+include("view/admin/modalValidationPromo.php");
 include("view/admin/modalProjet.php");
 include("view/admin/modalContact.php");
 
