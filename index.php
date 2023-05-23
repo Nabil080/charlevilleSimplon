90af806a899f79e902f9985758a2532efb887245
Florian Gaudin <120341138+Florian-Gaudin@users.noreply.github.com>
2023-05-23 11:21:30 +0200

    Changed user array to user object
    Pour obtenir le rôle user : modification du user en object

--- index.php
+++ index.php
@@ -2,17 +2,21 @@
 session_start();
 require_once 'src/controller/frontController.php';
 require 'src/controller/back/UserController.php';
-require 'src/model/ConnectBdd.php';
-
-$_SESSION['user'] = ['role' => 1];
-// var_dump($_SESSION);
 
 if (isset($_GET['action']) && $_GET['action'] !== '' && !isset($_GET['admin'])) {
     $action = $_GET['action'];
     require 'public.php';
 
     if (isset($_SESSION['user'])) {
-        if ($_SESSION['user']['role'] <= 3) {
+        if ($_SESSION['user']->role->id >=4) {
+            switch ($action) {
+                // Afficher son profil perso
+                case 'myProfile':
+                    myProfile();
+                    break;
+            }
+        }
+        else if ($_SESSION['user']->role->id <= 3) {
             switch ($action) {
                 // Afficher le CRUD de projet (Gestion de projet)
                 case 'projectGestionPage':
@@ -28,12 +32,12 @@ if (isset($_GET['action']) && $_GET['action'] !== '' && !isset($_GET['admin'])) 
                 // Envoi du formulaire d'ajoute de projet
                 // Envoi du formulaire de moficiation de projet
             }
-            if ($_SESSION['user']['role'] <= 2) {
+            if ($_SESSION['user']->role->id <= 2) {
                 switch ($action) {
                     // Envoie du formulaire de modification de projet (version modal)
                     // Envoi de la demande de suppression de projet
                 }
-                if ($_SESSION['user']['role'] == 1) {
+                if ($_SESSION['user']->role->id == 1) {
                     require 'admin.php';
                 }
             }
