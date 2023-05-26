95216a2f2ea051eee99ee33f0ff3931b8a24c987
Alexandre D <derriere.alexandre0103@gmail.com>
2023-05-26 15:50:15 +0200

    debut ajax

--- assets/js/all_projects.js
+++ assets/js/all_projects.js
@@ -1,71 +1,232 @@
-function searchByInputAndClassName(input,className)
-{
-    const search = input.value;
-    console.log(search);
-    var projects = document.getElementsByClassName(className);
-    console.log(projects);
-
-    for (var i = 0, len = projects.length; i < len; ++i) {
-
-        if(projects[i].innerHTML.toLowerCase().indexOf(search.toLowerCase()) === -1) {
-            projects[i].classList.add('!hidden');
-        }else{
-            projects[i].classList.remove('!hidden');
-        }
-    }
-}
-
 function toggleDropdown(id){
     const selectedDropdownButton = document.querySelector(`#${id}`)
-    const allDropdownButtons = document.querySelectorAll('.dropdown-toggle')
     const selectedDropdown = document.querySelector(`#${id} .filter-dropdown`)
-    const allDropdowns = document.querySelectorAll('.dropdown')
-
-
-    // // Réinitialise les autres dropdown
-    // allDropdownButtons.forEach(dropdownButton => {
-    //     if(dropdownButton.id != id){
-    //     dropdownButton.classList.add('rounded-b-lg')
-    //     }
-    // })
-    // allDropdowns.forEach(dropdown => dropdown.classList.add('hidden'))
-
     // Active le dropdown correspondant
     selectedDropdownButton.classList.toggle('rounded-b-lg')
     selectedDropdown.classList.toggle('hidden')
 }
 
-function showPageProjects(currentPage){
-    const projectsList = document.querySelectorAll('#project-cards article')
-    // console.log(projectsList)
-    const projectsCount = projectsList.length
-    const projectsPerPage = 6
-    const totalPages = Math.ceil(projectsCount/projectsPerPage)
-    const prevRange = ( currentPage - 1 ) * projectsPerPage
-    const nextRange = (currentPage * projectsPerPage ) - 1
+const projectGrid = document.querySelector('#project-cards');
+const paginationDiv = document.querySelector('#pagination');
+const prevPage = document.querySelector('#prev-page');
+const nextPage = document.querySelector('#next-page');
+const firstPage = document.querySelector('#first-page');
+const lastPage = document.querySelector('#last-page');
 
-    projectsList.forEach(e => e.classList.add('hidden','2xl:hidden'))
+const searchInput = document.querySelector('#project-search');
+const formationCheckboxes = document.querySelectorAll("#formation-dropdown input");
+const yearCheckboxes = document.querySelectorAll("#year-dropdown input");
+const levelCheckboxes = document.querySelectorAll("#level-dropdown input");
+const filterReset = document.querySelector('#filter-reset');
 
-    projectsList.forEach( (project, index) => {
-        if ( index >= prevRange && index <= nextRange){
-            project.classList.remove('hidden','2xl:hidden')
-        }
+let currentPage = 1
+
+const getProjets = () => {
+
+    return fetch('?action=projectsPagination')
+    .then((response) => response.text())
+    .then((data) => {
+        data = JSON.parse(data);
+
+        return data.projets
     })
+
 }
 
-showPageProjects(1)
+getProjets()
+.then((projets) => {
+    loadProjects(projets,1)
 
-const paginationList = document.querySelectorAll("#pagination div a")
-
-paginationList.forEach((pagination, index) => {
-    pagination.addEventListener('click',event => {
-        event.preventDefault()
-        paginationList.forEach((pagination, index) => {
-            pagination.classList.remove('bg-main-red','text-main-white')
+    formationCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', (e) => {
+            loadProjects(projets,1)
         })
-        pagination.classList.add('bg-main-red','text-main-white')
-        console.log(pagination)
-
-        showPageProjects(index+1)
     })
+
+    yearCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', (e) => {
+            loadProjects(projets,1)
+        })
+    })
+
+    levelCheckboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', (e) => {
+            loadProjects(projets,1)
+        })
+    })
+
+    searchInput.addEventListener('input', (e) => {
+        loadProjects(projets,1)
+    })
+
+    filterReset.addEventListener('click', (e) => {
+        document.querySelectorAll('input[type=checkbox]').forEach(checkbox => {
+            checkbox.checked = false
+        })
+        loadProjects(projets,1)
+    })
+
+    firstPage.addEventListener('click', (e) => {
+        loadProjects(projets,1)
+    })
+    prevPage.addEventListener('click', (e) => {
+        loadProjects(projets,currentPage - 1)
+    })
+
+    nextPage.addEventListener('click', (e) => {
+        loadProjects(projets,currentPage + 1)
+    })
+    lastPage.addEventListener('click', (e) => {
+        loadProjects(projets,'last')
+    })
+
 })
+.catch((error) => {
+    console.error(error);
+});
+
+
+function loadProjects(projets,number){
+
+    // * DEFINIT LES FILTRES FORMATION
+    const formationFilters = [];
+    formationCheckboxes.forEach(checkbox => {
+        // * AJOUTE LES CHECKBOX CHECK
+        if(checkbox.checked){
+            formationFilters.push(checkbox.value)
+        }
+    })
+    // * SI ARRAY VIDE GERE L'ERREUR :
+    if(formationFilters.length === 0){
+        formationFilters.push('')
+    }
+    // console.log(formationFilters)
+
+    // * DEFINIT LES FILTRES ANNÉES
+    const yearFilters = []
+    yearCheckboxes.forEach(checkbox => {
+        // * AJOUTE LES CHECKBOX CHECK
+        if(checkbox.checked){
+            yearFilters.push(checkbox.value)
+        }
+    })
+    // * SI ARRAY VIDE GERE L'ERREUR :
+    if(yearFilters.length === 0){
+        yearFilters.push('')
+    }
+    // console.log(yearFilters)
+
+    // * DEFINIT LES FILTRES NIVEAUX
+    const levelFilters = []
+    levelCheckboxes.forEach(checkbox => {
+        // * AJOUTE LES CHECKBOX CHECK
+        if(checkbox.checked){
+            levelFilters.push(checkbox.value)
+        }
+    })
+    // * SI ARRAY VIDE GERE L'ERREUR :
+    if(levelFilters.length === 0){
+        levelFilters.push('')
+    }
+    // console.log(levelFilters)
+
+    // ! CREATION DE L'ARRAY PROJETS FILTRES A PARTIR DE L'ARRAY TOUS LES PROJETS
+        // * CREATION DU NOUVEL ARRAY VIA .MAP
+        const filteredProjects = projets
+        // * FILTRES FORMATIONS
+        .filter((projet) => {
+            // ? exemple : const formationFilters = ['concepteurs développeurs', 'web', 'Référent'];
+            return formationFilters.some(filter =>
+            projet.toLowerCase().includes(filter.toLowerCase())
+            );
+        })
+        // * FILTRES ANNÉES
+        .filter((projet) => {
+            // ? exemple : const yearFilters = ['2023', '2022', '2021', '2018'];
+            return yearFilters.some(filter =>
+            projet.toLowerCase().includes(filter.toLowerCase())
+            );
+        })
+        // * FILTRES NIVEAUX
+        .filter((projet) => {
+            // ? exemple : const levelFilters = ['Bac+5', 'Bac+4', 'Bac+3', 'Bac+2', 'Bac+1'];
+            return levelFilters.some(filter =>
+            projet.toLowerCase().includes(filter.toLowerCase())
+            );
+        })
+        // * FILTRES RECHERCHE
+        .filter((projet) =>
+            projet.toLowerCase().includes(searchInput.value.toLowerCase())
+        )
+        .map((projet) => projet)
+
+
+    // * DEFINIT LE NOMBRE DE PROJETS FILTRES
+    const projectsCount = filteredProjects.length
+    // console.log(projectsCount)
+    // * DEFINT LE NOMBRE TOTAL DE PROJETS
+    const allProjectsCount = projets.length
+    // * AFFICHE LE NOMBRE DE PROJETS CORRESPONDANTS
+    projectGrid.innerHTML = `<h3 id="project-count" class="max-w-[766px] text-main-red mt-6 xl:col-span-2">${projectsCount} projets correspondants sur ${allProjectsCount}</h3>`;
+
+
+    // TODO: CREATION DE LA PAGINATION
+    // * VARIABLES POUR LA PAGINATION
+    let projectsPerPage = 4;
+    let pageCount = Math.ceil(projectsCount / projectsPerPage);
+    let paginationRange = 3
+    currentPage = number
+    if(number == 'last'){currentPage = pageCount}
+
+    const prevRange = ( currentPage - 1 ) * projectsPerPage
+    const nextRange = (currentPage * projectsPerPage ) 
+    // console.log(prevRange,nextRange)
+    console.log(currentPage)
+
+    
+    // * ACTIVE/DESACTIVE BOUTON PRECEDENT
+    if(currentPage == 1){
+        firstPage.classList.add('opacity-30', 'select-none', 'pointer-events-none')
+        prevPage.classList.add('opacity-30', 'select-none', 'pointer-events-none')
+    }else{
+        firstPage.classList.remove('opacity-30', 'select-none', 'pointer-events-none')
+        prevPage.classList.remove('opacity-30', 'select-none', 'pointer-events-none')
+    }
+
+    // * AFFICHE LA PAGINATION
+    paginationDiv.innerHTML = '';
+    for(page = 1; page <= pageCount; page++){
+        if(page > currentPage - paginationRange && page < currentPage + paginationRange){
+            if(page == currentPage){
+                paginationDiv.innerHTML += `<button id="${page}" class="bg-main-red text-main-white">${page}</button>`;
+            }else{
+                paginationDiv.innerHTML += `<button id="${page}" class="hover:bg-main-red hover:text-main-white">${page}</button>`;
+            }
+        }
+    }
+    // * AJOUTE LES EVENT LISTENERS
+    paginationDiv.querySelectorAll('button').forEach(button => 
+    {
+        button.addEventListener('click', (e) => {
+            console.log(Number(e.target.id))
+            loadProjects(projets,Number(e.target.id))
+        })
+    })
+
+    // * ACTIVE/DESACTIVE BOUTON SUIVANT
+    if(currentPage == pageCount){
+        lastPage.classList.add('opacity-30','select-none', 'pointer-events-none')
+        nextPage.classList.add('opacity-30', 'select-none', 'pointer-events-none')
+    }else{
+        lastPage.classList.remove('opacity-30','select-none', 'pointer-events-none')
+        nextPage.classList.remove('opacity-30', 'select-none', 'pointer-events-none')
+    }
+
+    // * LIMITE LES LES PROJETS AFFICHEES
+    const limitedProjects = filteredProjects.slice(prevRange,nextRange)
+    // * TRANSFORME L'ARRAY EN STRING ET L'INSERE
+    projectGrid.innerHTML += limitedProjects.join('');
+
+
+
+}
