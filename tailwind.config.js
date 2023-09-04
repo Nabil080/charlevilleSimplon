f2196997d59f539a2894012c70cd15b81b142adf
Alexandre D <derriere.alexandre0103@gmail.com>
2023-09-04 09:08:07 +0200

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
-  theme: {fontFamily: {
-    sans: 'Roboto',
-    title: 'Montserrat',
+  theme: {
+    fontFamily: {
+      sans: "Roboto",
+      title: "Montserrat",
     },
     extend: {
-        colors: {
-            'main':{
-                red:'#BD3124',
-                white:'#EFF1F3',
-                gray: '#4F4F4F',
-                lightred: '#F6DADE',
-                lightgray: '#F2F2F3',
-            }
-        }
-    }
+      colors: {
+        main: {
+          red: "#dc041c",
+          white: "#FFFF",
+          darkblue: "#045c7c",
+          lightblue: "#6ca4b4",
+          gray: "#4F4F4F",
+          lightred: "#F6DADE",
+          lightgray: "#F2F2F3",
+          blue: "#4A9AE6",
+          green: "#A2EF4D",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
